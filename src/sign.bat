d9b994688c4c99f9e70f16b0d455511c60ef25ad
Konstantin Vorobyev <kvorobyev@gmail.com>
2012-03-16 18:06:50 +0900

    Fix build scripts

--- src/sign.bat
+++ src/sign.bat
@@ -1,1 +1,1 @@
-C:\Home\Download\AdobeAIRSDK\bin\adt.bat -package -storetype pkcs12 -keystore ..\..\kostya.pfx -tsa none -target bundle bin dict.xml dict.html dict-sheet.html lib img lima1 dict
+C:\Home\Download\AdobeAIRSDK\bin\adt.bat -package -storetype pkcs12 -keystore ..\..\kostya.pfx -tsa none -target bundle .bin dict.xml dict.html dict-sheet.html lib img lima1 dict
